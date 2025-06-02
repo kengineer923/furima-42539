60580e6c7af72f9ee455e7fe87735b8a4d1b71e6
Kengineer <61613215+kengineer923@users.noreply.github.com>
2025-06-02 18:52:43 +0900

    トップページ、登録、ログインページ等への遷移実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # バリデーションを追加
+  validates :nickname, :last_name, :first_name, :last_kana, :first_kana, :birthday, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'items#index'
+  get 'posts', to: 'posts#index'
   devise_for :users
 end
