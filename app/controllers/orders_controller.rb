1956730528ece0c89f9b92ac7b4790c2b09d0845
Kengineer <61613215+kengineer923@users.noreply.github.com>
2025-06-13 17:55:18 +0900

    購入フォームと保存処理実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,9 +9,24 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      # PAY.JP支払い処理はSTEP2で実装
+      @order_address.save
+      return redirect_to root_path
+    else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY'] # エラーで再表示する際にも必要
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def order_address_params
+    params.require(:order_address).permit(
+      :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number
+    ).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
 
   def set_item
     @item = Item.find(params[:item_id])
